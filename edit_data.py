134966fa2b6fd39e6296ccd2c415abd4b2c5af7f
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-08-10 21:34:34 +0300

    Update edit_data.py

--- edit_data.py
+++ edit_data.py
@@ -1,22 +1,24 @@
 import os
 import json
 
+
 #получение имен файлов из базы данных + преобразованных имен без расширения
-def list_of_files() -> list:
-    directory = os.getcwd()+ "\\files"
+def list_of_files(path: str = 'files') -> tuple:
+    directory = os.getcwd() + path
     files = os.listdir(directory)
     json_files = list(filter(lambda x: x.endswith('.json'), files))
     files = [file[:-5] for file in json_files]
     return json_files, files
 
+
 companies_names, names_of_files = list_of_files()
 
 #получение всех видов данных для одного файла
-def get_features(file_name:str) -> set:
+def get_features(file_name:str, path: str = 'files') -> set:
     # поменяем директорию, если не находимся в папке с файлами о данных
     directory = os.getcwd()
-    if not directory.endswith("files"):
-        os.chdir(os.getcwd() + "\\files")
+    if not directory.endswith(path):
+        os.chdir(os.getcwd() + path)
 
     with open(file_name) as file:
         info_data = json.load(file)
@@ -61,7 +63,6 @@ def dict_feature_companies() -> dict:
 
 
 feature_company = dict_feature_companies()
-print(len(list(feature_company.keys())))
 
 #преобразованные названия данных
 new_features = {'Identifier_Cookie_or_similar_Tech_3rdParty': "Cookie or web-beacon files",
@@ -76,23 +77,23 @@ new_features = {'Identifier_Cookie_or_similar_Tech_3rdParty': "Cookie or web-bea
                 'Contact_3rdParty': "Your contact info",
                 'Demographic_Age_3rdParty': "Your age",
                 'Contact_Postal_Address_3rdParty':"Physical, home, work addresses",
-                'Contact_Phone_Number_3rdParty':"Your phone number",
+                'Contact_Phone_Number_3rdParty': "Your phone number",
                 'Contact_Address_Book_3rdParty': "Your phone book",
                 'Contact_ZIP_3rdParty': "Your area code",
-                'Contact_Password_3rdParty':"Your credential and passwords",
+                'Contact_Password_3rdParty': "Your credential and passwords",
                 "Location_WiFi_3rdParty": "Your wifi-location",
-                "Location_IP_Address_3rdParty":"Your IP Address",
-                "Location_GPS_3rdParty":"Your GPS location",
-                "Location_Cell_Tower_3rdParty":"Location of your network-base",
-                "Location_Bluetooth_3rdParty":"Detailed location of your bluetooth-device",
-                "Identifier_SSID_BSSID_3rdParty":"Wifi networks that you used",
-                "Identifier_SIM_Serial_3rdParty":"Your serial number",
-                "Identifier_Mobile_Carrier_3rdParty":"Your mobile oparator",
-                "Identifier_MAC_3rdParty":"Your hardware or device id",
-                "Identifier_IMSI_3rdParty":"Your international mobile subscriber",
-                "Identifier_IMEI_3rdParty":"Your international mobile equipment",
-                "Facebook_SSO":"Login credentials from one of your accounts",
-                "Demographic_Gender_3rdParty":"Your gender"
+                "Location_IP_Address_3rdParty": "Your IP Address",
+                "Location_GPS_3rdParty": "Your GPS location",
+                "Location_Cell_Tower_3rdParty": "Location of your network-base",
+                "Location_Bluetooth_3rdParty": "Detailed location of your bluetooth-device",
+                "Identifier_SSID_BSSID_3rdParty": "Wifi networks that you used",
+                "Identifier_SIM_Serial_3rdParty": "Your serial number",
+                "Identifier_Mobile_Carrier_3rdParty": "Your mobile oparator",
+                "Identifier_MAC_3rdParty": "Your hardware or device id",
+                "Identifier_IMSI_3rdParty": "Your international mobile subscriber",
+                "Identifier_IMEI_3rdParty": "Your international mobile equipment",
+                "Facebook_SSO": "Login credentials from one of your accounts",
+                "Demographic_Gender_3rdParty": "Your gender"
                }
 
 
@@ -104,5 +105,3 @@ def data_to_graph() -> dict:
     return feature_companies
 
 feature_companies = data_to_graph()
-
-
