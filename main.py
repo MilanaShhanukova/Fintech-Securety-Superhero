42cb5e0405404e7157a4d5b37a31735ba0b7374d
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-08-10 21:34:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from parser import *
 from handler import Handler
+from excel_handle import *
 
 if __name__ == '__main__':
-    current_file = open_yaml_files()
-    for entry in current_file:
-        company = Handler(entry)
+
+    unique_permissions = unique_features()
+
+    table_of_companies = ExcelHandler(unique_permissions)
+
+    privacy_files = open_policy_yaml_files()
+
+    for current_privacy_file in privacy_files:
+
+        company = Handler(current_privacy_file)
         company.handle_file()
-        data = company.get_permissions_data()
-        company_name = company.get_company_name()
-        save_file(company_name, data)
+
+        company_permissions, company_name = company.get_company_data()
+
+        table_of_companies.add_company_into_table(company_name, company_permissions)
+
+        save_file_as_json(company_name, company_permissions)
 
