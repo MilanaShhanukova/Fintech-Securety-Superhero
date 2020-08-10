c444c1e3ac6537fe4c24a3df4f6b0d090ec6cb26
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-08-10 21:34:29 +0300

    Update draw_grath.py

--- draw_grath.py
+++ draw_grath.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+
+import plotly.graph_objects as go
 import os
 import edit_data
 
@@ -18,6 +20,7 @@ user_feature = choose_feature()
 
 non_edit = []
 
+
 def edit_companies(list_companies: list) :
     length = len(list_companies)
 
@@ -33,9 +36,8 @@ def edit_companies(list_companies: list) :
         return non_edit
 
 
-
 def graph_feature(user_feature):
-    os.chdir(os.getcwd() +"\.." + "\\pictures")
+    # os.chdir(os.getcwd() + "/pictures")
 
     companies = list(feature_companies[user_feature])
 
@@ -53,23 +55,104 @@ def graph_feature(user_feature):
             graph.add_node(company)
             graph.add_edge(user_feature, company)
 
-            fig = plt.figure(figsize=(14, 14))
-            ax = plt.subplot(111)
-            ax.set_title(user_feature, fontsize=10)
+        pos = nx.spring_layout(graph, k=0.9, iterations=5000)
+        for n, p in pos.items():
+            graph.nodes[n]['pos'] = p
 
-            nx.draw(graph,
-                    node_size=6000,
-                    node_color="#FEDC56",
-                    with_labels=True,
-                    alpha=1,
-                    width=1.5,
-                    font_weight=570,
-                    edge_color="grey")
-            picture_name = "graph" + str(i) + ".png"
-            plt.savefig(picture_name, format="PNG")
+        edge_trace = go.Scatter(
+            x=[], y=[],
+            line=dict(width=4, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        for edge in graph.edges():
+            x0, y0 = graph.nodes[edge[0]]['pos']
+            x1, y1 = graph.nodes[edge[1]]['pos']
+            edge_trace['x'] += tuple([x0, x1, None])
+            edge_trace['y'] += tuple([y0, y1, None])
+
+        node_trace = go.Scatter(
+            x=[], y=[],
+            text=[],
+            textposition="middle center",
+            textfont=dict(
+                color="Black",
+                family="Gravitas One",
+                size=15,
+            ),
+            mode='markers+text',
+            hovertext=[],
+            hoverinfo='text',
+            marker=dict(
+                showscale=False,
+                color=["Green"],
+                size=[],
+                opacity=[1],
+                line_width=2))
+
+        for node in graph.nodes():
+            x, y = graph.nodes[node]['pos']
+            node_trace['x'] += tuple([x])
+            node_trace['y'] += tuple([y])
+
+        for node, adjacencies in enumerate(graph.adjacency()):
+            node_trace['marker']['color'] += tuple(["Yellow"])
+            node_info = adjacencies[0]
+            node_trace['text'] += tuple([node_info])
+            # node_trace['hovertext'] += tuple([])
+            node_trace['marker']['opacity'] += tuple([0.8])
+            node_trace['marker']['size'] += tuple([len(node_info) * 11])
+
+        fig = go.Figure(data=[edge_trace, node_trace],
+                        layout=go.Layout(
+                            title='<br>Граф компаний',
+                            titlefont_size=16,
+                            showlegend=False,
+                            hovermode='closest',
+                            margin=dict(b=20, l=5, r=5, t=40),
+                            annotations=[dict(
+                                text="Python code: <a href='https://github.com/LuckyDmitry/Fintech-Securety-Superhero/'> https://github.com/LuckyDmitry/Fintech-Securety-Superhero/</a>",
+                                showarrow=False,
+                                xref="paper", yref="paper",
+                                x=0.005, y=-0.002)],
+                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )
+        fig.show()
 
 
 clean_company = edit_data.no_data_companies
+
+
+# def clean_company_graph(clean_company):
+#     if len(clean_company) > 15:
+#         clean_company = edit_companies(clean_company)
+#         #print(parties)
+#     else:
+#         clean_company = [clean_company]
+#     os.chdir(os.getcwd() + "/pictures_of_clean_company")
+#
+#     for i in range(len(clean_company)):
+#         graph = nx.Graph()
+#         graph.add_node("Clean Company")
+#         for company in clean_company[i]:
+#             graph.add_node(company)
+#             graph.add_edge("Clean Company", company)
+#             fig = plt.figure(figsize=(14, 14))
+#             ax = plt.subplot(111)
+#             ax.set_title(user_feature, fontsize=10)
+#
+#             nx.draw(graph,
+#                     node_size=6000,
+#                     node_color="red",
+#                     with_labels=True,
+#                     alpha=1,
+#                     width=2,
+#                     font_weight=570,
+#                     edge_color="grey")
+#         picture_name = "graph" + str(i+1) + ".png"
+#         plt.savefig(picture_name, format="PNG")
+
 
 def clean_company_graph(clean_company):
     if len(clean_company) > 15:
@@ -77,30 +160,78 @@ def clean_company_graph(clean_company):
         #print(parties)
     else:
         clean_company = [clean_company]
-    os.chdir(os.getcwd() + "\.." + "\\pictures_of_clean_company")
+    # os.chdir(os.getcwd() + "/pictures_of_clean_company")
 
-    for i in range (len(clean_company)):
+    for i in range(len(clean_company)):
         graph = nx.Graph()
         graph.add_node("Clean Company")
         for company in clean_company[i]:
             graph.add_node(company)
             graph.add_edge("Clean Company", company)
-            fig = plt.figure(figsize=(14, 14))
-            ax = plt.subplot(111)
-            ax.set_title(user_feature, fontsize=10)
 
-            nx.draw(graph,
-                    node_size=6000,
-                    node_color="red",
-                    with_labels=True,
-                    alpha=1,
-                    width=2,
-                    font_weight=570,
-                    edge_color="grey")
-        picture_name = "graph" + str(i+1)+ ".png"
-        plt.savefig(picture_name, format="PNG")
+        pos = nx.spring_layout(graph, k=0.9, iterations=5000)
+        for n, p in pos.items():
+            graph.nodes[n]['pos'] = p
+
+        edge_trace = go.Scatter(
+            x=[], y=[],
+            line=dict(width=4, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        for edge in graph.edges():
+            x0, y0 = graph.nodes[edge[0]]['pos']
+            x1, y1 = graph.nodes[edge[1]]['pos']
+            edge_trace['x'] += tuple([x0, x1, None])
+            edge_trace['y'] += tuple([y0, y1, None])
+
+        node_trace = go.Scatter(
+            x=[], y=[],
+            text=[],
+            textposition="middle center",
+            textfont=dict(
+                color="Black",
+                family="Gravitas One",
+                size=15,
+            ),
+            mode='markers+text',
+            hoverinfo='text',
+            marker=dict(
+                showscale=False,
+                color=["Green"],
+                size=[],
+                opacity=[1],
+                line_width=2))
+
+        for node in graph.nodes():
+            x, y = graph.nodes[node]['pos']
+            node_trace['x'] += tuple([x])
+            node_trace['y'] += tuple([y])
+
+        for node, adjacencies in enumerate(graph.adjacency()):
+            node_trace['marker']['color'] += tuple(["Yellow"])
+            node_info = adjacencies[0]
+            node_trace['text'] += tuple([node_info])
+            node_trace['marker']['opacity'] += tuple([0.8])
+            node_trace['marker']['size'] += tuple([len(node_info) * 11])
 
 
+        fig = go.Figure(data=[edge_trace, node_trace],
+                        layout=go.Layout(
+                            title='<br>Граф компаний',
+                            titlefont_size=16,
+                            showlegend=False,
+                            hovermode='closest',
+                            margin=dict(b=20, l=5, r=5, t=40),
+                            annotations=[dict(
+                                text="Python code: <a href='https://github.com/LuckyDmitry/Fintech-Securety-Superhero/'> https://github.com/LuckyDmitry/Fintech-Securety-Superhero/</a>",
+                                showarrow=False,
+                                xref="paper", yref="paper",
+                                x=0.005, y=-0.002)],
+                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )
+        fig.show()
 
 clean_company_graph(clean_company)
 graph_feature(user_feature)
