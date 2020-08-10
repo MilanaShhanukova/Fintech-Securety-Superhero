11b56683b44d6d5aa77fd3685a3321652fd4b73a
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-08-10 21:33:10 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -9,28 +9,25 @@ class Handler:
         self._initial_data = initial_data
         self._company_name = str
 
-    def handle_file(self) -> None:
-        """
-        Parse a policy file. You'll get a company name and permissions and their description
+    def handle_file(self):
 
-        :return: None
-        """
         self.__set_company_name()
 
-        for i in self._initial_data['segments']:  # Handle each permission
+        for i in self._initial_data['segments']:
             self.__handle_data(i)
 
     def __set_company_name(self):
-        self._company_name = self._initial_data['policy_name']  # This stroke consists a company name in the file
+        self._company_name = self._initial_data['policy_name']
+
+    def get_company_name(self):
+        return self._company_name
 
     def __handle_data(self, data):
         for key, values in data.items():
             if (key == 'sentences') and values:
                 for i in values:
-                    permission = i['annotations'][0]['practice']
-                    if '3rdParty' in permission:
-                        all_permissions = [i['sentence_text'], permission]
-                        self._permissions.append(all_permissions)
+                    permission = [i['sentence_text'], i['annotations'][0]['practice']]
+                    self._permissions.append(permission)
 
-    def get_company_data(self) -> tuple:
-        return self._permissions, self._company_name
+    def get_permissions_data(self) -> list:
+        return self._permissions
